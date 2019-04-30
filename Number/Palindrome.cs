0775b8d71ab9a355c30378914dbf51e2a76e9d9c
Uğur Akıncı <ugurakinci@outlook.com>
2019-04-30 16:11:22 +0300

    Update Palindrome.cs

--- Number/Palindrome.cs
+++ Number/Palindrome.cs
@@ -1,17 +1,15 @@
 public bool IsPalindrome(int number)
-        {
-            if (number < 0 || (number % 10 == 0 && number != 0))
-            {
-                return false;
-            }
+{
+    if (number < 0 || (number % 10 == 0 && number != 0))
+    {
+        return false;
 
-            int revertedNumber = 0;
-            
-            while(number > revertedNumber)
-            {
-                revertedNumber = revertedNumber*10 + number % 10;
-                number /= 10;
-            }
+    int revertedNumber = 0;
+    
+    while(number > revertedNumber)
+    {
+        revertedNumber = revertedNumber*10 + number % 10;
+        number /= 10;
 
-            return number == revertedNumber || number == revertedNumber / 10;
-        }
+    return number == revertedNumber || number == revertedNumber / 10;
+}
