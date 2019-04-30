7fcb65e923a84a6d535e235cec0d29f879d39fae
Uğur Akıncı <ugurakinci@outlook.com>
2019-04-30 16:10:47 +0300

    Add files via upload

---
+++ Number/Palindrome.cs
@@ -0,0 +1,17 @@
+public bool IsPalindrome(int number)
+        {
+            if (number < 0 || (number % 10 == 0 && number != 0))
+            {
+                return false;
+            }
+
+            int revertedNumber = 0;
+            
+            while(number > revertedNumber)
+            {
+                revertedNumber = revertedNumber*10 + number % 10;
+                number /= 10;
+            }
+
+            return number == revertedNumber || number == revertedNumber / 10;
+        }
